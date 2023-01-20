6a60ca222a3ba2a4742705794e819635abb1bee7
ParadoxZW <964730078@qq.com>
2023-01-20 15:48:37 +0800

    Q3 bug fixed

--- chapter2_action/hw_submission/Q3/deploy.py
+++ chapter2_action/hw_submission/Q3/deploy.py
@@ -4,7 +4,8 @@ from ding.bonus import PPOF
 def rocket_landing():
     # Please install rocket env first, `pip3 install git+https://github.com/nighood/rocket-recycling@master#egg=rocket_recycling`
     agent = PPOF(env='rocket_landing', exp_name='./rocket_landing_deploy')
-    agent.deploy(enable_save_replay=True, ckpt_path='rocket_landing_ppo_demo/ckpt/final.pth.tar')
+    agent.policy.enable_mode.remove('learn')
+    agent.deploy(enable_save_replay=True, ckpt_path='rocket_landing_ppo_demo/ckpt/eval.pth.tar')
 
 if __name__ == "__main__":
     rocket_landing()
