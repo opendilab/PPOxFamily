71e2ea2a176a6faf812fb4e6ce90dc0e06473197
karroyan <karroyan@sjtu,edu,cn>
2023-02-27 18:51:27 +0800

    add comment of popart and popart_demo

--- chapter4_reward/popart.py
+++ chapter4_reward/popart.py
@@ -1,19 +1,23 @@
-from typing import Optional, Union
+"""
+Implementation of ``POPART`` algorithm for reward rescale.
+<link https://arxiv.org/abs/1602.07714 link>
 
+POPART is an adaptive normalization algorithm to normalized the targets used in the learning updates. The two main components in POPART are:
+**ART**: to update scale and shift such that the return is appropriately normalized
+**POP**: to preserve the outputs of the unnormalized function when we change the scale and shift.
+
+"""
+from typing import Optional, Union
 import math
 import torch
 import torch.nn as nn
 
 
 class PopArt(nn.Module):
-    r"""
-    Overview:
+    """
+    **Overview**:
         A linear layer with popart normalization.
-    Interfaces:
-        forward
-    .. note:
-        For more popart info, you can refer to
-        the paper <https://arxiv.org/abs/1809.04474>
+        For more popart implementation info, you can refer to the paper <link https://arxiv.org/abs/1809.04474 link>
     """
 
     def __init__(
@@ -22,15 +26,17 @@ class PopArt(nn.Module):
             output_features: Union[int, None] = None,
             beta: float = 0.5
     ) -> None:
-
+        # PyTorch necessary requirements for extending ``nn.Module`` . Our network should also subclass this class.
         super(PopArt, self).__init__()
-
+        
         self.beta = beta
         self.input_features = input_features
         self.output_features = output_features
+        # Initialize the linear layer parameters, weight and bias.
         self.weight = nn.Parameter(torch.Tensor(output_features, input_features))
         self.bias = nn.Parameter(torch.Tensor(output_features))
-
+        # Register a buffer for normalization parameters which can not be considered as model parameters.
+        # The normalization parameters will be used later to save the target value's scale and shift.
         self.register_buffer('mu', torch.zeros(output_features, requires_grad=False))
         self.register_buffer('sigma', torch.ones(output_features, requires_grad=False))
         self.register_buffer('v', torch.ones(output_features, requires_grad=False))
@@ -38,6 +44,11 @@ class PopArt(nn.Module):
         self.reset_parameters()
 
     def reset_parameters(self):
+        # In Kaiming Initialization, the mean of weights increment slowly and the std is close to 1, which avoid the vanishing gradient problem and exploding gradient problem of deep models.
+        # Specifically, the Kaiming Initialization funciton is as follows:
+        # $$std = \sqrt{\frac{2}{(1+a^2)\times fan\_in}}$$
+        # where a is the the negative slope of the rectifier used after this layer (0 for ReLU by default), and fan_in is the number of input dimension.
+        # For more kaiming intialization info, you can refer to the paper <link https://arxiv.org/pdf/1502.01852.pdf link>
         nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
         if self.bias is not None:
             fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
@@ -45,58 +56,57 @@ class PopArt(nn.Module):
             nn.init.uniform_(self.bias, -bound, bound)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        r"""
-            Overview:
-                Return the popart output.
-            Arguments:
-                - x (:obj:`torch.Tensor`): The input tensor.
-            Returns:
-                - x (:obj:`torch.Tensor`): The output tensor.
-            """
-
+        """
+        **Overview**:
+            The computation of the linear layer, which outputs both the output and the normalized output of the layer.
+        """
         normalized_output = x.mm(self.weight.t())
         normalized_output += self.bias.unsqueeze(0).expand_as(normalized_output)
-
+        # The unnormalization of output
         with torch.no_grad():
             output = normalized_output * self.sigma + self.mu
 
-        return normalized_output
+        return output, normalized_output
 
     def update_parameters(self, value):
-        r"""
-            Overview:
-                Update the popart parameters.
-            Arguments:
-                - value (:obj:`torch.Tensor`): The input q value tensor.
-            Returns:
-                - output (:obj:`Dict`): The dict of popart normalization parameters.
-            """
-
+        """
+        **Overview**:
+            The parameters update, which outputs both the output and the normalized output of the layer.
+        """
+        # Tensor device conversion of the normalization parameters.
         self.mu = self.mu.to(value.device)
         self.sigma = self.sigma.to(value.device)
         self.v = self.v.to(value.device)
 
         old_mu = self.mu
         old_std = self.sigma
-        old_v = self.v
-
+        # Calculate the first and second moments (mean and variance) of the target value:
+        # $$\mu = \frac{G_t}{B}$$
+        # $$v = \frac{G_t^2}{B}$$.
         batch_mean = torch.mean(value, 0)
         batch_v = torch.mean(torch.pow(value, 2), 0)
-
+        # Replace the nan value with old value.
         batch_mean[torch.isnan(batch_mean)] = self.mu[torch.isnan(batch_mean)]
         batch_v[torch.isnan(batch_v)] = self.v[torch.isnan(batch_v)]
-
+        # Soft update the normalization parameters according to:
+        # $$\mu_t = (1-\beta)\mu_{t-1}+\beta G^v_t$$
+        # $$v_t = (1-\beta)v_{t-1}+\beta(G^v_t)^2$$.
         batch_mean = (1 - self.beta) * self.mu + self.beta * batch_mean
         batch_v = (1 - self.beta) * self.v + self.beta * batch_v
-
+        # Calculate the standard deviation with the mean and variance:
+        # $$\sigma = \sqrt{v-\mu^2}$$
         batch_std = torch.sqrt(batch_v - (batch_mean ** 2))
+        # Clip the standard deviation to reject the outlier data.
         batch_std = torch.clamp(batch_std, min=1e-4, max=1e+6)
+        # Replace the nan value with old value.
         batch_std[torch.isnan(batch_std)] = self.sigma[torch.isnan(batch_std)]
 
         self.mu = batch_mean
         self.v = batch_v
         self.sigma = batch_std
-
+        # Update weight and bias with mean and standard deviation to preserve unnormalised outputs:
+        # $$w'_i = \frac{\sigma_i}{\sigma'_i}w_i$$
+        # $$b'_i = \frac{\sigma_i b_i + \mu_i-\mu'_i}{\sigma'_i}$$
         self.weight.data = (self.weight.t() * old_std / self.sigma).t()
         self.bias.data = (old_std * self.bias + old_mu - self.mu) / self.sigma
 
--- chapter4_reward/popart_demo.py
+++ chapter4_reward/popart_demo.py
@@ -1,3 +1,7 @@
+"""
+A demo for popart.py, implement a supervised learning model to fit the Q value of lunarlander.
+The network is a MLP with popart as the final layer.
+"""
 from typing import Dict
 import pickle
 import torch
@@ -6,14 +10,20 @@ from torch.optim import Adam
 from torch.utils.data import DataLoader
 from popart import PopArt
 
-class MLPNetwork(nn.Module):
+class MLP(nn.Module):
 
     def __init__(self, obs_shape: int, action_shape: int) -> None:
         """
-        **Overview**：
-            A MLP network used to fit the Q value.
+        **Overview**:
+            A MLP network with popart as the final layer.
+            Input: observations and actions
+            Output: Estimated Q value
+            ``cat(obs,actions) -> encoder -> popart`` .
         """
-        super(MLPNetwork, self).__init__()
+        super(MLP, self).__init__()
+        # Define the encoder and popart layer.
+        # Here we use MLP with two layer and ReLU as activate function.
+        # The final layer is popart.
         self.encoder = nn.Sequential(
             nn.Linear(obs_shape+action_shape, 16),
             nn.ReLU(),
@@ -21,51 +31,53 @@ class MLPNetwork(nn.Module):
             nn.ReLU(),
         )
         self.popart = PopArt(32, 1)
-        #self.decoder = nn.Linear(32, 1)
 
     def forward(self, obs: torch.Tensor, actions: torch.Tensor) -> torch.Tensor:
+        # The encoder first concatenate the observation vectors and actions,
+        # then map the input to an embedding vector.
         x = torch.cat((obs, actions),1)
         x = self.encoder(x)
+        # The popart layer maps the embedding vector to a normalized value.
         normalized_output  = self.popart(x)
         return normalized_output 
 
-def train(obs_shape: int, action_shape: int, train_data):
-    model = MLPNetwork(obs_shape, action_shape)
+
+def train(obs_shape: int, action_shape: int, NUM_EPOCH: int, train_data):
+    model = MLP(obs_shape, action_shape)
     optimizer = Adam(model.parameters(), lr=0.0001, weight_decay=0.0001)
     MSEloss = nn.MSELoss()
+    # Read the preprocessed data of trained agent on lunarlander.
+    # Data format: list of dict with keys: ``{'observations', 'actions', 'rewards'}`` , 
+    # where the rewards is the discounted return from the current state. 
     train_data = DataLoader(train_data, batch_size = 64, shuffle = True)
-    for epoch in range(2000):
+
+    for epoch in range(NUM_EPOCH):
         for idx, data in enumerate(train_data):
             optimizer.zero_grad()
-            normalized_output = model(data['observations'], data['actions'])
+            # Compute the normalized output and the 
+            output, normalized_output = model(data['observations'], data['actions'])
             mu = model.popart.mu
             sigma = model.popart.sigma
-
+            # Normalize the target return to align with the normalized Q value.
             with torch.no_grad():
                 normalized_reward = (data['rewards'] - mu)/sigma
-
-            # print(normalized_output, normalized_reward, mu, sigma)
-
+            # The loss is calculated as the MSE loss between normalized Q value and normalized target return.
             loss = MSEloss(normalized_output, normalized_reward)
-            # if epoch % 100 == 99:   
-            #     print(normalized_output, normalized_reward)
             loss.backward()
             optimizer.step()
-
+            # After the model parameters are updated with the gradient, the weights and bias should be updated to preserve unnormalised outputs.
             model.popart.update_parameters(data['rewards'])
 
             running_loss = 0.0
-            running_loss += loss.item()     # extract the loss value
+            running_loss += loss.item()
+
         if epoch % 100 == 99:    
-            # print every 1000 
-            print('Epoch [%d] loss: %.6f' %
-                    (epoch + 1, running_loss / 100))
-            # zero the loss
+            print('Epoch [%d] loss: %.6f' % (epoch + 1, running_loss / 100))
             running_loss = 0.0
             
 if __name__ == '__main__':
     f = open('processed_data.pkl', 'rb')
     dataset = pickle.load(f)
-    train(obs_shape=8, action_shape=1, train_data=dataset)
+    train(obs_shape=8, action_shape=1, NUM_EPOCH = 2000, train_data=dataset)
 
     
