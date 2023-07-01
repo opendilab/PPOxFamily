ab21c7f699928cc6df9b9f7b95e3a1a60c2921a9
‘whl’ <18231213@buaa.edu.cn>
2023-07-01 19:39:34 +0800

    feature(whl): add demo code for orthogonal init

---
+++ chapter7_tricks/orthogonal_init.py
@@ -0,0 +1,69 @@
+"""
+Official implementation of orthogonal initialization using PyTorch.
+"""
+import torch
+
+
+def orthogonal_(tensor, gain=1):
+    """
+    **Overview:**
+        Fills the input `Tensor` with a (semi) orthogonal matrix, as described in this paper <link https://arxiv.org/pdf/1312.6120.pdf link>.
+        The input tensor must have at least 2 dimensions, and for tensors with more than 2 dimensions the trailing dimensions are flattened.
+    """
+    if tensor.ndimension() < 2:
+        raise ValueError("Only tensors with 2 or more dimensions are supported")
+    
+    # Initialize a new tensor with normal distribution. The shape is the same as the input tensor.
+    rows = tensor.size(0)
+    cols = tensor.numel() // rows
+    flattened = tensor.new(rows, cols).normal_(0, 1)
+    
+    # If rows < cols, transpose the original tensor for computational efficiency.
+    if rows < cols:
+        flattened.t_()
+
+    # Compute the qr factorization, Q is an orthogonal matrix and R is an upper triangular matrix.
+    q, r = torch.linalg.qr(flattened)
+    # Although q is orthogonal, each value of q is not uniformly distributed. To make Q uniform, we can use the equation below: $$Q^* = Q sign(diag(R))$$. Proof for this equation can be viewed in this paper: <link https://arxiv.org/pdf/math-ph/0609050.pdf link>.
+    d = torch.diag(r, 0)
+    ph = d.sign()
+    q *= ph
+    
+    # If rows < cols, transpose the output tensor to match the shape of original tensor.
+    if rows < cols:
+        q.t_()
+    
+    # Reshape the result and multiply an optional scaling factor.
+    with torch.no_grad():
+        tensor.view_as(q).copy_(q)
+        # Multiply an optional scaling factor.
+        tensor.mul_(gain)
+    return tensor
+
+
+# delimiter
+def test_orthogonal() -> None:
+    """
+    **Overview**:
+        Test the `orthogonal_` function.
+    """
+    # For Conv weights.
+    w1 = torch.empty((4, 4, 3, 3))
+    orthogonal_(w1)
+    # Test the result.
+    w1 = w1.reshape(w1.shape[0], -1).T
+    res = w1.T @ w1
+    gt = torch.eye(w1.shape[1])
+    assert torch.sum((res - gt) ** 2).item() < 1e-9
+
+    # For Linear weights.
+    w2 = torch.empty((4, 4))
+    orthogonal_(w2)
+    # Test the result.
+    res = w2.T @ w2
+    gt = torch.eye(w2.shape[1])
+    assert torch.sum((res - gt) ** 2).item() < 1e-9
+
+
+if __name__ == "__main__":
+    test_orthogonal()
