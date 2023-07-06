29a6e86b7d64f4e2842a6a7a48cc306f5309dfc6
‘whl’ <18231213@buaa.edu.cn>
2023-07-06 11:27:58 +0800

    polish according to comments

--- chapter7_tricks/orthogonal_init.py
+++ chapter7_tricks/orthogonal_init.py
@@ -4,7 +4,7 @@ Official implementation of orthogonal initialization using PyTorch.
 import torch
 
 
-def orthogonal_(tensor, gain=1):
+def orthogonal_(tensor: torch.Tensor, gain: float = 1) -> torch.Tensor:
     """
     **Overview:**
         Fills the input `Tensor` with a (semi) orthogonal matrix, as described in this paper <link https://arxiv.org/pdf/1312.6120.pdf link>.
@@ -33,8 +33,9 @@ def orthogonal_(tensor, gain=1):
     if rows < cols:
         q.t_()
     
-    # Reshape the result and multiply an optional scaling factor.
+    # Using `torch.no_grad()` there can make sure that these operations won't be added to the computational graph used by PyTorch's autograd system, thus improving efficiency.
     with torch.no_grad():
+        # Reshape the result and copy the weight from q.
         tensor.view_as(q).copy_(q)
         # Multiply an optional scaling factor.
         tensor.mul_(gain)
@@ -45,12 +46,12 @@ def orthogonal_(tensor, gain=1):
 def test_orthogonal() -> None:
     """
     **Overview**:
-        Test the `orthogonal_` function.
+        Test the `orthogonal_` function. We use a weight tensor of convolutional layer and a weight tensor of linear layer as test cases, and check whether the results are correctly orthogonalized.
     """
-    # For Conv weights.
+    # For Conv. weights.
     w1 = torch.empty((4, 4, 3, 3))
     orthogonal_(w1)
-    # Test the result.
+    # Test whether the result is orthogonal.
     w1 = w1.reshape(w1.shape[0], -1).T
     res = w1.T @ w1
     gt = torch.eye(w1.shape[1])
@@ -59,7 +60,7 @@ def test_orthogonal() -> None:
     # For Linear weights.
     w2 = torch.empty((4, 4))
     orthogonal_(w2)
-    # Test the result.
+    # Test whether the result is orthogonal.
     res = w2.T @ w2
     gt = torch.eye(w2.shape[1])
     assert torch.sum((res - gt) ** 2).item() < 1e-9
