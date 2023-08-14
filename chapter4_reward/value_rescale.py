126902f0ccca23e777e7f0bbdb1478e8f8306bb2
niuyazhe <niuyazhe@sensetime.com>
2023-08-14 20:24:10 +0800

    feature(nyz): add ch4 value rescale demo code and ch6/ch7 translation

--- chapter4_reward/value_rescale.py
+++ chapter4_reward/value_rescale.py
@@ -1,60 +1,60 @@
 """
-**Overview:**
 Typically, we need to apply normalization functions in RL training to reduce the scale of some predictions of neural networks (e.g. value function) to enhance the RL training process.
 In this document, we will demonstrate two kinds of data normalization methods and their corresponding inverse operations.
-- The first one is ``value_transform``, which can reduce the scale of the action-value function. Its corresponding inverse operation is ``value_inv_transform``.
-- The second one is ``symlog``, which is another approach to normalize the input tensor. Its corresponding inverse operation is ``inv_symlog``.
+- The first one is ``value_transform`` , which can reduce the scale of the action-value function. Its corresponding inverse operation is ``value_inv_transform`` . <link https://arxiv.org/pdf/1805.11593.pdf link>
+- The second one is ``symlog`` , which is another approach to normalize the input tensor. Its corresponding inverse operation is ``inv_symlog`` . <link https://arxiv.org/pdf/2301.04104.pdf link>
 """
 import torch
 
 
 def value_transform(x: torch.Tensor, eps: float = 1e-2) -> torch.Tensor:
     """
-    **Overview:**
+    **Overview**:
         A function to reduce the scale of the action-value function. For extensive reading, please refer to: Achieving Consistent Performance on Atari <link https://arxiv.org/abs/1805.11593 link>
-        Given the input tensor ``x``, this function will return the normalized tensor.
+        Given the input tensor ``x`` , this function will return the normalized tensor.
         The argument ``eps`` is a hyper-parameter that controls the additive regularization term to ensure the corresponding inverse operation is Lipschitz continuous.
-        The formula of the normalization is: $$h(x) = sign(x)(\sqrt{(abs(x)+1)} - 1) + \eps * x$$
     """
+    # Core implementation.
+    # The formula of the normalization is: $$h(x) = sign(x)(\sqrt{(|x|+1)} - 1) + \epsilon * x$$
     return torch.sign(x) * (torch.sqrt(torch.abs(x) + 1) - 1) + eps * x
 
 
 # delimiter
 def value_inv_transform(x: torch.Tensor, eps: float = 1e-2) -> torch.Tensor:
     """
-    **Overview:**
-        The inverse form of value transform. Given the input tensor ``x``, this function will return the unnormalized tensor.
-        The formula of the unnormalization is: $$`h^{-1}(x) = sign(x)({(\frac{\sqrt{1+4\eps(|x|+1+\eps)}-1}{2\eps})}^2-1)$$
+    **Overview**:
+        The inverse form of value transform. Given the input tensor ``x`` , this function will return the unnormalized tensor.
     """
+    # The formula of the unnormalization is: $$h^{-1}(x) = sign(x)({(\frac{\sqrt{1+4\epsilon(|x|+1+\epsilon)}-1}{2\epsilon})}^2-1)$$
     return torch.sign(x) * (((torch.sqrt(1 + 4 * eps * (torch.abs(x) + 1 + eps)) - 1) / (2 * eps)) ** 2 - 1)
 
 
 # delimiter
 def symlog(x: torch.Tensor) -> torch.Tensor:
     """
-    **Overview:**
+    **Overview**:
         A function to normalize the targets. For extensive reading, please refer to: Mastering Diverse Domains through World Models <link https://arxiv.org/abs/2301.04104 link>
-        Given the input tensor ``x``, this function will return the normalized tensor.
-        The formula of the normalization is: $$symlog(x) = sign(x)(\ln{|x|+1})$$
+        Given the input tensor ``x`` , this function will return the normalized tensor.
     """
+    # The formula of the normalization is: $$symlog(x) = sign(x)(\ln{|x|+1})$$
     return torch.sign(x) * (torch.log(torch.abs(x) + 1))
 
 
 # delimiter
 def inv_symlog(x: torch.Tensor) -> torch.Tensor:
     """
-    **Overview:**
-        The inverse form of symlog. Given the input tensor ``x``, this function will return the unnormalized tensor.
-        The formula of the unnormalization is: $$symexp(x) = sign(x)(\exp{|x|}-1)$$
+    **Overview**:
+        The inverse form of symlog. Given the input tensor ``x`` , this function will return the unnormalized tensor.
     """
+    # The formula of the unnormalization is: $$symexp(x) = sign(x)(\exp{|x|}-1)$$
     return torch.sign(x) * (torch.exp(torch.abs(x)) - 1)
 
 
 # delimiter
 def test_value_transform():
     """
-    **Overview:**
-        Generate fake data and test the ``value_transform`` ``value_inv_transform`` functions.
+    **Overview**:
+        Generate fake data and test the ``value_transform`` and ``value_inv_transform`` functions.
     """
     # Generate fake data.
     test_x = torch.randn(10)
@@ -70,8 +70,8 @@ def test_value_transform():
 # delimiter
 def test_symlog():
     """
-    **Overview:**
-        Generate fake data and test the ``symlog`` ``inv_symlog`` functions.
+    **Overview**:
+        Generate fake data and test the ``symlog`` and ``inv_symlog`` functions.
     """
     # Generate fake data.
     test_x = torch.randn(10)
--- chapter6_marl/mappo_zh.py
+++ chapter6_marl/mappo_zh.py
@@ -1,14 +1,15 @@
 """
-PyTorch基础集中式训练和分散式执行（CTDE）MAPPO算法的教程，适用于多智能体合作场景。
-本教程使用 ``marl_network`` 中定义的 CTDEActorCriticNetwork 和 ``pg`` 中定义的损失函数。主要函数使用构造的测试数据描述了 CTDE MAPPO 算法的核心部分。
+PyTorch基础集中式训练和分布式执行（CTDE）MAPPO 算法的教程，适用于多智能体合作场景。
+本教程使用 ``marl_network`` 中定义的 CTDEActorCriticNetwork 和 ``ppo`` 中定义的损失函数，并结合 ``gae`` 中定义的优势函数计算方法。
+另外，主函数使用构造的测试数据描述了 CTDE MAPPO 算法的核心部分。
 关于多智能体合作强化学习的更多细节可以在 <link https://github.com/opendilab/PPOxFamily/blob/main/chapter6_marl/chapter6_lecture.pdf link> 中找到。
 """
 
 import torch
 from marl_network import CTDEActorCriticNetwork
-# 需要复制 chapter1_overview 中 ppo 的实现
+# 需要复制 chapter1_overview 中 ppo 的实现到当前目录
 from ppo import ppo_policy_data, ppo_policy_error
-# 需要复制 chapter7_tricks 中 gae 的实现
+# 需要复制 chapter7_tricks 中 gae 的实现到当前目录
 from gae import gae
 
 
@@ -16,32 +17,32 @@ from gae import gae
 def mappo_training_opeator() -> None:
     """
     **概述**
-    这是关于 CTDE PPO 算法训练过程的主要函数。
-    首先，定义一些超参数，神经网络和优化器，然后生成构造的测试数据并计算演员-评论家损失 (actor-critic loss)。
-    最后，使用优化器更新网络参数。在实际应用中，训练数据应该是由环境进行交互得到的。
-    注意在本文件中，策略网络指的是演员 (actor)，价值网络指的是评论家 (critic)。
+    这是关于 CTDE MAPPO 算法训练过程的核心函数。
+    首先，定义一些超参数，神经网络和优化器，然后生成构造的测试数据并计算演员-评论家损失 (Actor-Critic loss)。
+    最后，使用优化器更新网络参数。在实际应用中，训练数据应该是由环境进行在线交互得到的。
+    注意在本文件中，策略网络指的是演员 (Actor)，价值网络指的是评论家 (Critic)。
     """
     # 设置必要的超参数。
     batch_size, agent_num, local_state_shape, agent_specific_global_state_shape, action_shape = 4, 5, 10, 25, 6
     # 熵加成权重，有利于探索。
     entropy_weight = 0.001
-    # 价值损失权重，旨在平衡损失规模。
+    # 价值损失权重，旨在平衡不同损失函数量级。
     value_weight = 0.5
     # 未来奖励的折扣系数。
     discount_factor = 0.99
-    # 根据运行环境设置tensor设备为cuda或者cpu。
+    # 根据运行环境设置 tensor 设备为 cuda 或者 cpu。
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     # 定义多智能体神经网络和优化器。
     model = CTDEActorCriticNetwork(agent_num, local_state_shape, agent_specific_global_state_shape, action_shape)
     model.to(device)
-    # Adam 是深度强化学习中最常用的优化器。 如果你想添加权重衰减机制，应该使用``torch.optim.AdamW``。
+    # Adam 是深度强化学习中最常用的优化器。 如果你想添加权重衰减机制，应该使用 ``torch.optim.AdamW`` 。
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-    # 定义相应的测试数据，遵循与与环境交互的数据格式相同。
-    # 注意，数据应该与网络保持相同的设备 (device)。
-    # 为简单起见，我们将整个批次数据视为一个完整的 episode。
-    # 在实际应用中，训练批次是多个回合的组合。我们通常使用 ``done`` 变量来划分不同的回合。
+    # 定义相应的测试数据，需要保持数据格式与环境交互生成的数据格式相同。
+    # 注意，数据应该与网络保持相同的计算设备 (device)。
+    # 为简单起见，这里我们将整个批次数据视为一个完整的 episode。
+    # 在实际应用中，训练批次是多个 episode 的组合。我们通常使用 ``done`` 变量来划分不同的 episode 。
     local_state = torch.randn(batch_size, agent_num, local_state_shape).to(device)
     agent_specific_global_state = torch.randn(batch_size, agent_num, agent_specific_global_state_shape).to(device)
     logit_old = torch.randn(batch_size, agent_num, action_shape).to(device)
@@ -50,20 +51,20 @@ def mappo_training_opeator() -> None:
     done[-1] = 1
     action = torch.randint(0, action_shape, (batch_size, agent_num)).to(device)
     reward = torch.randn(batch_size, agent_num).to(device)
-    # Return_ 可以用不同的方法计算。这里我们使用奖励的折扣累计金额。
-    # 还可以使用广义优势估计 (GAE) 法、n 步回归法等。
+    # 目标回报可以用不同的方法计算。这里我们使用奖励的折扣累计值。
+    # 还可以使用广义优势估计 (GAE) 法、n-step TD 方法等等。
     return_ = torch.zeros_like(reward)
     for i in reversed(range(batch_size)):
         return_[i] = reward[i] + (discount_factor * return_[i + 1] if i + 1 < batch_size else 0)
 
-    # Actor-critic 网络前向传播。
+    # Actor-Critic 网络前向传播。
     output = model(local_state, agent_specific_global_state)
     # ``squeeze`` 操作将 shape 从 $$(B, A, 1)$$ 转化为 $$(B, A)$$.
     value = output.value.squeeze(-1)
     # 使用广义优势估计（Generalized Advantage Estimation，简称GAE）方法来计算优势（Advantage）。
-    # 优势是策略损失的一种“权重”，因此它被包含在``torch.no_grad()``中，表示不进行梯度计算。
-    # ``done``是回合结束的标志。``traj_flag``是轨迹（trajectory）的标志。
-    # 在这里，我们将整个批次数据视为一个完整的回合，所以``done``和``traj_flag``是相同的。
+    # 优势是策略损失的一种“权重”，因此它被包含在 ``torch.no_grad()`` 中，表示不进行梯度计算。
+    # ``done`` 是回合结束的标志。``traj_flag`` 是轨迹（trajectory）的标志。
+    # 在这里，我们将整个批次数据视为一个完整的回合，所以 ``done`` 和 ``traj_flag`` 是相同的。
     with torch.no_grad():
         traj_flag = done
         gae_data = (value, value_old, reward, done, traj_flag)
